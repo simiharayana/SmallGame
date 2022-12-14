9a4ad6d6f898a3f86faa7fc1b720883cb542afd3
Samiksha <110822794+simiharayana@users.noreply.github.com>
2022-12-14 17:01:04 +0530

    Add files via upload

---
+++ firstGame.js
@@ -0,0 +1,70 @@
+// // var readlineSync = require("readline-sync");
+
+
+// // var score = 0;
+
+// // var questions = [{
+// //   question: "from where simi belong? ",
+// //   answer: "Harayana"
+// // }, {
+// //   question: "who is favourit superhero of simi ? ",
+// //   answer: "Dev"
+// // },
+// // {
+// //   question: "Where she is doing study? ",
+// //   answer: "Pune"
+// // },{
+// //   question:"what is her favourite colour? ",
+// //   answer:"Black"
+// // },{
+// //   question:"who is her best friend? ",
+// //   answer:"pooja"
+// // }];
+
+
+// // function welcome() {
+// // var userName = readlineSync.question("What's your name ? ");
+
+// //   console.log("Welcome "+ userName + " do you know about  Simii?");
+// // }
+
+
+
+// // function play(question, answer) {
+// //   var yourAnswer = readlineSync.question(question);
+
+// //   if (yourAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+// //     console.log("right!");
+// //     score = score + 1;
+    
+// //   } else {
+// //     console.log("wrong!");
+   
+// //   }
+
+// //   console.log("current score: ", score);
+// //   console.log("***************")
+// // }
+
+// // function startgame() {
+// //   for (var i=0; i<questions.length; i++) {
+// //     var aboutMe = questions[i];
+// //     play(aboutMe.question, aboutMe.answer)
+// //   }
+// // }
+
+// // function result() {
+// //   console.log("YAY! You SCORED: ", score);
+// //   console.log("Thanks for playing!!!")
+// // }
+  
+// // welcome();
+// // startgame();
+// // result();
+
+// var a=prompt("enter one number: ")
+// if (a>0){
+//   console.log("positive number: ")
+// }else{
+//   console.log("nagative number")
+// }
